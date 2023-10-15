8fe42f7b039c1229f52a4e3f16873c00c0286ad9
GodSamble <osgneb8359@naver.com>
2023-10-15 20:56:11 +0900

    RxSwift 예제1 실습하며 익히기

--- RxExample/RxExample/Examples/Numbers/NumbersViewController.swift
+++ RxExample/RxExample/Examples/Numbers/NumbersViewController.swift
@@ -14,16 +14,19 @@ class NumbersViewController: ViewController {
     @IBOutlet weak var number1: UITextField!
     @IBOutlet weak var number2: UITextField!
     @IBOutlet weak var number3: UITextField!
-
+    @IBOutlet weak var number4: UITextField!
     @IBOutlet weak var result: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        Observable.combineLatest(number1.rx.text.orEmpty, number2.rx.text.orEmpty, number3.rx.text.orEmpty) { textValue1, textValue2, textValue3 -> Int in
-                return (Int(textValue1) ?? 0) + (Int(textValue2) ?? 0) + (Int(textValue3) ?? 0)
-            }
-            .map { $0.description }
+        
+        Observable.combineLatest(number1.rx.text.orEmpty,
+                                 number2.rx.text.orEmpty,
+                                 number3.rx.text.orEmpty,
+                                 number4.rx.text.orEmpty) { textValue1, textValue2, textValue3, textValue4 -> Int in
+                return (Int(textValue1) ?? 0) + (Int(textValue2) ?? 0) + (Int(textValue3) ?? 0) + (Int(textValue4) ?? 0)
+            } //Int로 들어왔는데
+            .map { $0.description } //String으로 형변환함.
             .bind(to: result.rx.text)
             .disposed(by: disposeBag)
     }
