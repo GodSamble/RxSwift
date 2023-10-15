b13acd49725fc350ca4778e2db8e46c8ab2e5513
GodSamble <osgneb8359@naver.com>
2023-10-15 21:16:29 +0900

    RxSwift 예제2 실습하며 익히기

--- RxExample/RxExample/Examples/SimpleValidation/SimpleValidationViewController.swift
+++ RxExample/RxExample/Examples/SimpleValidation/SimpleValidationViewController.swift
@@ -37,11 +37,15 @@ class SimpleValidationViewController : ViewController {
             .map { $0.count >= minimalPasswordLength }
             .share(replay: 1)
 
-        let everythingValid = Observable.combineLatest(usernameValid, passwordValid) { $0 && $1 }
+        let everythingValid = Observable.combineLatest(usernameValid, passwordValid) { $0 && $1 } // 파이프라인 연결해서 UI가 연결되도록 구성함.
             .share(replay: 1)
 
+        // TODO: map 으로 일일이 하는 것이 아닌, share을 쓰는이유
+        // TODO: 구독행위(bind, 혹은 Subscribe)를 해주고, map 추적을 해보면 개수대로 뜸..
+        // TODO: 그래서 share 가 도입되어, 한 번만 쓰고도 모든 곳에 적용이 됨.
+        
         usernameValid
-            .bind(to: passwordOutlet.rx.isEnabled)
+            .bind(to: passwordOutlet.rx.isEnabled) // 이렇게 연결되어있음
             .disposed(by: disposeBag)
 
         usernameValid
